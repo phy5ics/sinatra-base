31da7e02ac069f0388a9160ec49d24764a34b2c4
John Barton <jb@phy5ics.com>
2013-04-03 17:55:03 -0400

    Added compass support. Added render_file helper. Added dynamic compass renderer

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra/flash'
 require "sinatra/reloader" if development?
 require 'haml'
 require 'sass'
+require 'compass'
 require 'mongoid'
 
 set :environment, ENV['RACK_ENV']
@@ -13,13 +14,36 @@ configure do
 		Mongoid.load!('config/mongoid.yml')
 		# Mongoid.logger = nil
 	end
+	
+	Compass.configuration do |config|
+		config.project_path = File.dirname __FILE__
+		config.sass_dir = 'views/css'
+	end
+
+	set :sass, Compass.sass_engine_options
 end
 
-module BASE_APP
+module Base
   class App < Sinatra::Base
    
     get '/' do
+			@js = ['/js/jquery-1.9.1.min.js']
+			@css = ['/css/global.css']
       haml :index
+    end
+
+		# ---------------------------------------------------------
+		# Helpers
+		#	---------------------------------------------------------
+	
+		get '/css/:name.css' do
+			content_type 'text/css', :charset => 'utf-8'
+			sass :"css/#{params[:name]}", Compass.sass_engine_options
+		end
+		
+		def render_file(filename)
+      contents = File.read('views/' + filename)
+      Haml::Engine.new(contents).render
     end
   
   end
